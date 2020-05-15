65d5dc5522262ab61dc656d18995cfcd27165e4f
Alex K <contact.alexsophia@gmail.com>
2020-05-15 16:40:43 +0300

    Added more elements to the page.

--- assets/js/script_saved_todos.js
+++ assets/js/script_saved_todos.js
@@ -7,17 +7,55 @@ function createToDo(inputText) {
     let todoElementContainer = document.createElement("div");
     todoElementContainer.setAttribute("class", "todo_element_container");
 
+    let todoCheckbox = document.createElement("div");
+    todoCheckbox.setAttribute("class", "todo_checkbox");
+
+    let closeButton = document.createElement("div");
+    closeButton.setAttribute("class", 'closebutton');
+    closeButton.innerHTML = 'del';
+
+    closeButton.addEventListener('click', event => {
+            event.preventDefault();
+            event.stopPropagation();
+
+            todoElement.setAttribute("class", 'todo_element deleted');
+
+            setTimeout(function () {
+                todoElement.remove();
+                removeFromLocalStorage(inputText);
+            }, 200);
+        }
+    )
+
     let todoElementText = document.createElement("div");
     todoElementText.setAttribute("class", "todo_element_text");
 
     todoElementText.innerHTML = inputText;
 
+    todoElementContainer.appendChild(todoCheckbox);
     todoElementContainer.appendChild(todoElementText);
+    todoElementContainer.appendChild(closeButton);
+
     todoElement.appendChild(todoElementContainer);
+
     listContainer.appendChild(todoElement);
 
 }
 
+function removeFromLocalStorage(inputText) {
+    let savedTodos = JSON.parse(localStorage.getItem("completed_todos"));
+
+    if (savedTodos) {
+        savedTodos.some(function (oneTodoText, index) {
+            if (oneTodoText === inputText) {
+                savedTodos.splice(index, 1);
+                return true;
+            }
+        });
+        localStorage.setItem('completed_todos', JSON.stringify(savedTodos));
+
+    }
+}
 
 function extractFromLocalStorage() {
     let savedTodos = JSON.parse(localStorage.getItem("completed_todos"));
