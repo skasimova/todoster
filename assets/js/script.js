cf59d41dbd9b6ec0fbfa305087507595cae9e876
Alex K <contact.alexsophia@gmail.com>
2020-05-19 15:04:00 +0300

    Refactored the script.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,221 @@
-let form = document.getElementById('form');
-let inputForm = document.getElementById('creation_field');
+class Storage {
+    constructor(storageKey) {
+        this.storage = localStorage;
+        this.storageKey = storageKey;
+    }
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    createTask(inputForm.value);
+    getTasks() {
+        let tasks = [];
 
-    saveToLocalStorage(inputForm.value);
+        if (this.storage.getItem(this.storageKey) !== null) {
+            tasks = JSON.parse(this.storage.getItem(this.storageKey));
+        }
 
-    inputForm.value = '';
-});
+        return tasks;
+    }
 
-let taskList = document.getElementById('task_list');
+    saveTasks(tasks) {
+        this.storage.setItem(this.storageKey, JSON.stringify(tasks));
+    }
 
-function createTask(inputText) {
-    let task = document.createElement('div');
-    task.setAttribute('class', 'task');
+    addTask(taskText) {
+        let tasks = this.getTasks();
 
-    task.addEventListener('click', event => {
+        tasks.push(taskText);
+
+        this.saveTasks(tasks);
+    }
+
+    deleteTask(taskText) {
+        let tasks = this.getTasks();
+
+        if (tasks) {
+            tasks.some(function (currentTaskText, index) {
+                if (currentTaskText === taskText) {
+                    tasks.splice(index, 1);
+                    return true;
+                }
+            });
+
+            this.saveTasks(tasks);
+        }
+    }
+}
+
+const storage = new Storage('added_tasks');
+const storageCompleted = new Storage('completed_tasks');
+
+let tasksPage = document.getElementById('tasks_page');
+
+if (tasksPage !== null) {
+    let form = document.getElementById('form');
+    let inputForm = document.getElementById('creation_field');
+
+    form.addEventListener('submit', event => {
         event.preventDefault();
+        createTaskOnFirstPage(inputForm.value);
 
-        task.setAttribute('class', 'task completed');
+        storage.addTask(inputForm.value);
 
-        setTimeout(function () {
-            task.remove();
+        inputForm.value = '';
+    });
 
-            removeFromLocalStorage(inputText);
+    let taskList = document.getElementById('task_list');
 
-            saveToSecondLocalStorage(inputText);
-        }, 300);
-    })
+    function createTaskOnFirstPage(inputText) {
+        let task = document.createElement('div');
+        task.setAttribute('class', 'task');
 
-    let taskArea = document.createElement('div');
-    taskArea.setAttribute('class', 'task_area');
-
-    let checkbox = document.createElement('div');
-    checkbox.setAttribute('class', 'task_check');
-
-    let deleteButton = document.createElement('div');
-    deleteButton.setAttribute('class', 'task_delete');
-    deleteButton.innerHTML = 'del';
-
-    deleteButton.addEventListener('click', event => {
+        task.addEventListener('click', event => {
             event.preventDefault();
-            event.stopPropagation();
 
-            task.setAttribute('class', 'task deleted');
+            task.setAttribute('class', 'task completed');
 
             setTimeout(function () {
                 task.remove();
-                removeFromLocalStorage(inputText);
-            }, 200);
-        }
-    )
 
-    let taskText = document.createElement('div');
-    taskText.setAttribute('class', 'task_text');
+                storage.deleteTask(inputText);
+                storageCompleted.addTask(inputText);
+            }, 300);
+        })
 
-    taskText.innerHTML = inputText;
+        let taskArea = document.createElement('div');
+        taskArea.setAttribute('class', 'task_area');
 
-    taskArea.appendChild(checkbox);
-    taskArea.appendChild(taskText);
-    taskArea.appendChild(deleteButton);
+        let checkbox = document.createElement('div');
+        checkbox.setAttribute('class', 'task_check');
 
-    task.appendChild(taskArea);
+        let deleteButton = document.createElement('div');
+        deleteButton.setAttribute('class', 'task_delete');
+        deleteButton.innerHTML = 'del';
 
-    taskList.appendChild(task);
-}
+        deleteButton.addEventListener('click', event => {
+                event.preventDefault();
+                event.stopPropagation();
 
+                task.setAttribute('class', 'task deleted');
 
-function saveToSecondLocalStorage(inputText) {
-
-    let savedTasksArray;
-
-    if (localStorage.getItem('completed_tasks') === null) {
-        savedTasksArray = [];
-    } else {
-        savedTasksArray = JSON.parse(localStorage.getItem('completed_tasks'));
-    }
-
-    savedTasksArray.push(inputText);
-
-    localStorage.setItem('completed_tasks', JSON.stringify(savedTasksArray));
-}
-
-function saveToLocalStorage(inputText) {
-
-    let savedTasksArray;
-
-    if (localStorage.getItem('added_tasks') === null) {
-        savedTasksArray = [];
-    } else {
-        savedTasksArray = JSON.parse(localStorage.getItem('added_tasks'));
-    }
-
-    savedTasksArray.push(inputText);
-
-    localStorage.setItem('added_tasks', JSON.stringify(savedTasksArray));
-}
-
-
-function removeFromLocalStorage(inputText) {
-    let savedTasks = JSON.parse(localStorage.getItem('added_tasks'));
-
-    if (savedTasks) {
-        savedTasks.some(function (oneTaskText, index) {
-            if (oneTaskText === inputText) {
-                savedTasks.splice(index, 1);
-                return true;
+                setTimeout(function () {
+                    task.remove();
+                    storage.deleteTask(inputText);
+                }, 200);
             }
-        });
-        localStorage.setItem('added_tasks', JSON.stringify(savedTasks));
+        )
+
+        let taskText = document.createElement('div');
+        taskText.setAttribute('class', 'task_text');
+
+        taskText.innerHTML = inputText;
+
+        taskArea.appendChild(checkbox);
+        taskArea.appendChild(taskText);
+        taskArea.appendChild(deleteButton);
+
+        task.appendChild(taskArea);
+
+        taskList.appendChild(task);
     }
+
+    function extractFromLocalStorageFirstPage() {
+        let tasks = storage.getTasks();
+
+        if (tasks) {
+            tasks.forEach(function (oneTaskText) {
+                createTaskOnFirstPage(oneTaskText);
+            });
+        }
+    }
+
+    extractFromLocalStorageFirstPage();
 }
 
-function extractFromLocalStorage() {
-    let savedTasks = JSON.parse(localStorage.getItem('added_tasks'));
+let completedTasksPage = document.getElementById('completed_tasks_page');
 
-    if (savedTasks) {
-        savedTasks.forEach(function (oneTaskText) {
-            createTask(oneTaskText);
-        });
+if (completedTasksPage !== null) {
+    let taskList = document.getElementById('task_list');
+
+    function completedCount() {
+        let countedElements = document.getElementsByClassName('task').length;
+        let counter = document.getElementById('completed_count');
+
+        counter.innerText = countedElements === 0 ? 'none' : countedElements;
     }
+
+    function createTaskOnSecondPage(inputText) {
+        let task = document.createElement('div');
+        task.setAttribute('class', 'task');
+
+        let taskArea = document.createElement("div");
+        taskArea.setAttribute('class', 'task_area');
+
+        let undoButton = document.createElement('div');
+        undoButton.setAttribute('class', 'task_undo');
+        undoButton.innerHTML = 'undo';
+
+        undoButton.addEventListener('click', event => {
+            event.preventDefault();
+            event.stopPropagation();
+
+            task.setAttribute('class', 'task undone');
+
+            setTimeout(function () {
+
+                task.remove();
+
+                storageCompleted.deleteTask(inputText);
+                storage.addTask(inputText);
+
+                completedCount();
+            }, 300);
+        })
+
+        let deleteButton = document.createElement("div");
+        deleteButton.setAttribute('class', 'task_delete');
+        deleteButton.innerHTML = 'del';
+
+        deleteButton.addEventListener('click', event => {
+                event.preventDefault();
+                event.stopPropagation();
+
+                task.setAttribute('class', 'task deleted');
+
+                setTimeout(function () {
+                    task.remove();
+
+                    storageCompleted.deleteTask(inputText);
+
+                    completedCount();
+
+                }, 200);
+            }
+        )
+
+        let taskText = document.createElement('div');
+        taskText.setAttribute('class', 'task_text');
+        taskText.innerHTML = inputText;
+
+        taskArea.appendChild(undoButton);
+        taskArea.appendChild(taskText);
+        taskArea.appendChild(deleteButton);
+
+        task.appendChild(taskArea);
+
+        taskList.appendChild(task);
+
+    }
+
+    function extractFromLocalStorageSecondPage() {
+        let tasks = storageCompleted.getTasks();
+
+        if (tasks) {
+            tasks.forEach(function (oneTaskText) {
+                createTaskOnSecondPage(oneTaskText);
+            });
+        }
+
+        completedCount();
+
+    }
+
+    extractFromLocalStorageSecondPage();
 }
-
-extractFromLocalStorage();
-
